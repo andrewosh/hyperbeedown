5a7a2d91fa66f9692c1e27e29a0618d45ca8239e
Andrew Osheroff <andrewosh@gmail.com>
2020-09-03 11:12:19 +0200

    Add example

---
+++ example.js
@@ -0,0 +1,19 @@
+const HyperbeeDown = require('.')
+const Hyperbee = require('hyperbee')
+const ram = require('random-access-memory')
+const hypercore = require('hypercore')
+const levelup = require('levelup')
+
+start ()
+
+async function start () {
+  const core = hypercore(ram)
+  const tree = new Hyperbee(core)
+  await tree.ready()
+
+  const down = new HyperbeeDown(tree)
+  const db = levelup(down)
+
+  await db.put('hello', 'world')
+  console.log(await db.get('hello'))
+}
